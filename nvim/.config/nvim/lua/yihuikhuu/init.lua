298335ef86fcecf444b0445178f8d420b3aa5e68
Yihui Khuu <yihui.khuu@gmail.com>
2023-12-10 19:53:03 +1030

    initial commit

---
+++ nvim/.config/nvim/lua/yihuikhuu/init.lua
@@ -0,0 +1,3 @@
+require("yihuikhuu.remap")
+require("yihuikhuu.set")
+require("yihuikhuu.lazy")
---
+++ nvim/.config/nvim/lua/yihuikhuu/lazy.lua
@@ -0,0 +1,52 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope.nvim", version = "0.1.4", dependencies = { "nvim-lua/plenary.nvim" } },
+    { "rose-pine/neovim",              name = "rose-pine" },
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    },
+    { "nvim-treesitter/nvim-treesitter",        build = ":TSUpdate" },
+    { "theprimeagen/harpoon",                   branch = "harpoon2", dependencies = { "nvim-lua/plenary.nvim" } },
+    { "mbbill/undotree" },
+    { "tpope/vim-fugitive" },
+    { "nvim-treesitter/nvim-treesitter-context" },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
+        lazy = true,
+        config = false,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            { "hrsh7th/cmp-nvim-lsp" },
+        }
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            { "L3MON4D3/LuaSnip" }
+        },
+    },
+}, opts)
