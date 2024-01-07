341710fef96514c92199a905905cabf99339a224
Yihui Khuu <yihui.khuu@gmail.com>
2024-01-07 10:44:55 +1030

    add ghosty. update install/cleanup script

--- clean-env.sh
+++ clean-env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+source ./env.sh
+
 pushd $DOTFILES
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
 do
--- env.sh
+++ env.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env zsh
 
 if [[ -z $STOW_FOLDERS ]]; then
-    STOW_FOLDERS="nvim,tmux,zsh"
+    STOW_FOLDERS="nvim,tmux,zsh,ghostty"
 fi
 
 if [[ -z $DOTFILES ]]; then
     DOTFILES=$HOME/.dotfiles
 fi
-
-STOW_FOLDERS=$STOW_FOLDERS DOTFILES=$DOTFILES $DOTFILES/install.sh	
