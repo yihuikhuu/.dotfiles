8c3ee7aaaff1e8d8af16f984dfb5264fa6340767
Yihui Khuu <yihui.khuu@gmail.com>
2024-01-28 19:37:00 +1030

    fix flashing statusline issue when dragging visual selection

--- nvim/.config/nvim/lua/yihuikhuu/remap.lua
+++ nvim/.config/nvim/lua/yihuikhuu/remap.lua
@@ -2,8 +2,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Move selected lines up and down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
