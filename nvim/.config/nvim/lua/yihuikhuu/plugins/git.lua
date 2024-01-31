b886ccbb7ff170d8752bbc3310f148f6a1506f9d
Yihui Khuu <yihui.khuu@gmail.com>
2024-01-31 22:51:36 +1030

    add git plugins
    oops

---
+++ nvim/.config/nvim/lua/yihuikhuu/plugins/git.lua
@@ -0,0 +1,12 @@
+return {
+    { "tpope/vim-fugitive" },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
+
+            vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>")
+            vim.keymap.set("n", "<leader>gt", "<cmd>Gitsigns toggle_current_line_blame<CR>")
+        end
+    }
+}
