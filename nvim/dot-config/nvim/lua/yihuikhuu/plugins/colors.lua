9df4b78aec9a6467700ae34b9b516a28807f130a
Yihui Khuu <yihui.khuu@gmail.com>
2025-02-21 20:05:07 +1100

    update line number colour for better contrast

--- nvim/dot-config/nvim/lua/yihuikhuu/plugins/colors.lua
+++ nvim/dot-config/nvim/lua/yihuikhuu/plugins/colors.lua
@@ -5,6 +5,14 @@ return {
         priority = 1000,
         opts = {},
         config = function()
+            require("tokyonight").setup({
+                style = "night",
+                on_highlights = function(highlights, colors)
+                    highlights.LineNrAbove = { fg = "#5c6570" }
+                    highlights.LineNr = { fg = "#5c6570" }
+                    highlights.LineNrBelow = { fg = "#5c6570" }
+                end,
+            })
             vim.cmd.colorscheme "tokyonight-night"
         end
     }
