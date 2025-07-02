69cc87ed57c5f6dc7faadd4f7e089258dae28c9e
Yihui Khuu <yihui.khuu@gmail.com>
2025-07-02 22:50:27 +1000

    add new utils and aliases

--- zsh/dot-config/zsh/include/aliases.zsh
+++ zsh/dot-config/zsh/include/aliases.zsh
@@ -1,10 +1,21 @@
+# Zsh
 alias s='source ~/.config/zsh/.zshrc'
 
-alias ll='ls -al'
-alias l='ls -CF'
+# File system
+alias l='eza -lh --group-directories-first --icons=auto'
+alias ll='l -a'
+alias lt='eza --tree --level=2 --long --icons --git'
+alias lta='lt -a'
+alias ff="fzf --preview 'bat --style=numbers --color=always {}'"
+alias cd="z"
 
-alias bi='brew install '
-alias bu='brew upgrade'
-
+# Directories
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
 alias w='cd ~/work'
 alias dot='cd ~/.dotfiles'
+
+# Homebrew
+alias bi='brew install '
+alias bu='brew upgrade'
---
+++ zsh/dot-config/zsh/include/init.zsh
@@ -0,0 +1,5 @@
+# Set up fzf key bindings and fuzzy completion
+source <(fzf --zsh)
+
+# Set up zoxide
+eval "$(zoxide init zsh)"
