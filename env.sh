5fb6d1d0b9b3e0e7c5bb5917c84f2976d5838bba
Yihui Khuu <yihui.khuu@gmail.com>
2025-09-05 23:47:48 +1000

    add karabiner

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 if [[ -z $STOW_FOLDERS ]]; then
-    STOW_FOLDERS="nvim,tmux,zsh,ghostty"
+    STOW_FOLDERS="nvim,tmux,zsh,ghostty,karabiner"
 fi
 
 if [[ -z $DOTFILES ]]; then
